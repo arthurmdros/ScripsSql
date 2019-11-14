05012d812f8d8cc21b820c6416420f3fc15a2dd8
arthurmdros <arthurmedeiros2005@gmail.com>
2019-11-14 13:11:42 -0300

    Adicionando Script de Povoamento

---
+++ Script_Povoamento.sql
@@ -0,0 +1,31 @@
+USE empresa;
+
+INSERT INTO alocacao VALUES('001','1','100');
+INSERT INTO alocacao VALUES('002','3','90');
+INSERT INTO alocacao VALUES('003','3','300');
+INSERT INTO alocacao VALUES('004','2','260');
+INSERT INTO alocacao VALUES('005','4','130');
+
+INSERT INTO departamento VALUES('1','Ciência','4','2000-02-10');
+INSERT INTO departamento VALUES('2','Matemática','1','2000-02-10');
+INSERT INTO departamento VALUES('3','Tecnologia','2','2000-02-10');
+INSERT INTO departamento VALUES('4','História','3','2000-02-10');
+INSERT INTO departamento VALUES('5','Artes','3','2000-02-10');
+
+INSERT INTO dependente VALUES('1','001','dependente01','F');
+INSERT INTO dependente VALUES('2','002','dependente02','M');
+INSERT INTO dependente VALUES('3','003','dependente03','M');
+INSERT INTO dependente VALUES('4','004','dependente04','M');
+INSERT INTO dependente VALUES('5','005','dependente01','F');
+
+INSERT INTO empregado VALUES('001','empregado01','rua dos prazeres','1000.00','5','2');
+INSERT INTO empregado VALUES('002','empregado02','rua dos palmares','1000.00','3','5');
+INSERT INTO empregado VALUES('003','empregado03','rua do fim do mundo','1000.00','4','1');
+INSERT INTO empregado VALUES('004','empregado04','rua do não te interessa','1000.00','4','3');
+INSERT INTO empregado VALUES('005','empregado05','rua do cruzado no queixo','1000.00','2','4');
+
+INSERT INTO projeto VALUES('1','Projeto1','Caicó-RN',2);
+INSERT INTO projeto VALUES('2','Projeto2','Assu-RN',1);
+INSERT INTO projeto VALUES('3','Projeto3','Natal-RN',1);
+INSERT INTO projeto VALUES('4','Projeto4','Natal-RN',4);
+INSERT INTO projeto VALUES('5','Projeto5','Caicó-RN',5);
